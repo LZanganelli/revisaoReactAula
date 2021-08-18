80d3d1ed0bb8fa4ff0c38cbc651e0ca019eabda8
Lucas Zanganelli <lucasfzanganelli@gmail.com>
2021-08-18 19:25:07 -0300

    Alteraçoes
    Alterei o app e o package json

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card } from 'react-native-paper';
 import Menu from './components/Menu';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Menu/>
+      <Card/>
     </View>
   );
 }
